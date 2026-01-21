0c0305b5f45212db467d22daee2d99a5f153e30e
Easar <e59577087@gmail.com>
2026-01-21 21:49:56 +0800

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -1,6 +1,10 @@
 const Application = require('@waline/vercel');
 
 module.exports = Application({
+  // 在此处显式覆盖数据库配置，确保不被任何默认设置影响
+  database: {
+    type: process.env.DATABASE_TYPE || 'postgresql', // 显式指定类型，环境变量优先
+  },
   plugins: [],
   async postSave(comment) {
     // do what ever you want after comment saved
